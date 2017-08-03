eb51e8b78a8a7d3ddabdc187624106bbf2cbc9db
Andy Smith <andy@nsnw.ca>
2017-08-03 10:57:20 -0700

    Further capability parsing

--- aprslib/parsing/capability.py
+++ aprslib/parsing/capability.py
@@ -29,10 +29,28 @@ def parse_capability(body):
 
     # Loop through the capabilities
     for pair in match.group(2).rstrip().split(','):
-        (token, value) = pair.split('=')
-        capabilities.update({
-            token: value
-        })
+        try:
+            (token, value) = pair.split('=')
+            capabilities.update({
+                token: value
+            })
+        except ValueError:
+            # There's two possibilities here:-
+            try:
+                # 1. The last field is a comment and has a ',' in it
+                # - for this, append the whole thing, comma and all, to the
+                # previous one.
+                capabilities.update({
+                    token: '{},{}'.format(value, pair)
+                })
+            except UnboundLocalError:
+                # 2. The format is something like: <IGATE,CLOSED
+                # - in this case, there's no 'previous' token for us to append to,
+                # so we can just add a single blank entry with CLOSED as the
+                # token.
+                capabilities.update({
+                    pair: None
+                })
 
     # Add capabilities to the returned parsed dict
     parsed.update({
